affa9540af62494c029497bff1e871d38578713f
Juliya Pekhota <konyliyaevg@gmail.com>
2022-03-17 13:48:59 +0300

    updated img path

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import imageLoad from '../../images/load.svg';
+import ImageLoad from '../../images/load.svg';
 
 import s from './Loader.module.css';
 
 export const Loader: FC = () => {
     return (
         <div className={s.wrap}>
-            Загрузка <img className={s.image} src={imageLoad} alt='loading'/>
+            Загрузка <ImageLoad />
         </div>
     )
 }
